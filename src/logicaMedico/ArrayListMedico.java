53a6d532490370da4837c515525816d3d51db64b
erickbm2002 <eriickbarriientos42@gmail.com>
2025-06-22 02:19:00 -0600

    Rescate de mis cambios locales

---
+++ src/logicaMedico/ArrayListMedico.java
@@ -0,0 +1,34 @@
+package logicaMedico;
+
+import java.util.ArrayList;
+
+public class ArrayListMedico {
+
+    private ArrayList<Object> listaMedicos;
+
+    
+
+    public ArrayListMedico(){
+        this.listaMedicos = new ArrayList<>();
+    }
+
+    public void agregarMedico(Object medico) {
+        this.listaMedicos.add(medico);
+    }
+
+    public void mostrarDatosLista() {
+        
+    }
+
+    public ArrayList<Object> getListaMedicos() {
+        return listaMedicos;
+    }
+
+    public void setListaMedicos(ArrayList<Object> listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+
+    
+
+}
+ 
